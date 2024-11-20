6bf42401e613a2c9bb295f2276c605fc7d49fb6b
Jim <jimsrobot@users.noreply.github.com>
2024-11-21 08:28:19 +1100

    test 2

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome')->name('home');
+Route::view('/', 'welcome')->name('homepage');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
